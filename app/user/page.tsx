4b8c4aeb0b5ba9a6dae54e2f8cfef83d46bf9c88
Jeremias Koch <jkoch@griddynamics.com>
2025-07-28 09:39:32 -0300

    feat: add route level layout, error and logs pages, remove legacy store

---
+++ app/user/page.tsx
@@ -0,0 +1,12 @@
+import { AddUser, UserList } from '@/components';
+
+const UserPage = () => {
+  return (
+    <div className="w-fit">
+      <AddUser />
+      <UserList />
+    </div>
+  );
+};
+
+export default UserPage;
